7ec7ba40dafbc9ef4784e6a521a16e4edb9a09d2
Czarek Stu <czarstur@gmail.com>
2018-11-24 03:19:11 +0100

    Changed from unitest to py.test

--- twitter_test.py
+++ twitter_test.py
@@ -1,21 +1,10 @@
-import unittest
-
 from twitter import Twitter
 
-class TwitterTest(unittest.TestCase):
-    def setUp(self):
-        self.twitter = Twitter()
+def test_initization():
+    twitter = Twitter()
+    assert twitter
 
-    def test_initialization(self):
-        self.assertTrue(self.twitter)
-
-    def test_twitter_single(self):
-        #GIVEN
-                #WHEN
-        self.twitter.tweet('Test message')
-        #THEN
-        self.assertEqual(self.twitter.tweets, ['Test message'])
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_message():
+    twitter = Twitter()
+    twitter.tweet('dupa')
+    assert twitter.tweets == ['dupa']
